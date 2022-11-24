79a822bc1ee0982a16db360eb63cc9df8ad0a5ea
mahiuddinTuhin <mahiuddiinntuhin@gmail.com>
2022-11-24 22:42:29 +0600

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = process.env.PORT;
 
